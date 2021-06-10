6dc677a4a36c2f9a7ae1464038c0de13c12da096
harumonia <zxjlm233@gmail.com>
2021-06-10 12:27:03 +0800

    init

--- source/js/ui/banner.js
+++ source/js/ui/banner.js
@@ -22,21 +22,35 @@ function generateBanner(title) {
   let sumH = 0;
   let lineTop = document.querySelector(".vertical-line-top");
   let lineBottom = document.querySelector(".vertical-line-bottom");
+
+  let tmp = title.split("-");
+  title = [tmp[0], " ", ...tmp[1]];
+
   for (let i = 0; i < title.length; i++) {
     const char = title[i];
     let charBox = document.createElement("div");
     let rn = random(1.5, 3.5);
-    charBox.innerHTML = "<span class='char'>" + char + "</span>";
+    if (char === " ") {
+      charBox.innerHTML =
+        `<span class='char' style='height: ${rn / 2}rem'>` + char + "</span>";
+    } else {
+      charBox.innerHTML = "<span class='char'>" + char + "</span>";
+    }
+
     let charSize = rn + "rem";
     banner.insertBefore(charBox, lineBottom);
     charBox.classList.add("char-box");
-    if (i % 2 === 0) {
-      charBox.classList.add("char-left");
-      charBox.style.animationName = "char-move-left";
-    } else {
-      charBox.classList.add("char-right");
-      charBox.style.animationName = "char-move-right";
+
+    if (i !== 0) {
+      if (i % 2 === 0) {
+        charBox.classList.add("char-left");
+        charBox.style.animationName = "char-move-left";
+      } else {
+        charBox.classList.add("char-right");
+        charBox.style.animationName = "char-move-right";
+      }
     }
+
     charBox.style.setProperty("--banner-char-size", charSize);
 
     const width = window
